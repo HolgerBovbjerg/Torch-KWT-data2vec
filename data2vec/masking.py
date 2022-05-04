30bf6bd273941fe0446b2c4c0f8794f63b1069de
hbovbjer <holger.severin.bovbjerg@cern.com>
2022-05-04 13:11:25 +0000

    add data2vec folder

---
+++ data2vec/masking.py
@@ -0,0 +1,193 @@
+"""
+Transforms and masking strategies for Self Supervised Pretraining of Vision models.
+All codes copied from below repos:
+https://github.com/microsoft/unilm/tree/master/beit
+https://github.com/rwightman/pytorch-image-models/tree/master/timm
+https://github.com/facebookresearch/deit
+"""
+import warnings
+import random
+import math
+from PIL import Image
+import numpy as np
+import torch
+from torchvision import transforms
+import torchvision.transforms.functional as F
+from timm.data.constants import (IMAGENET_DEFAULT_MEAN, IMAGENET_DEFAULT_STD,
+                                 IMAGENET_INCEPTION_MEAN, IMAGENET_INCEPTION_STD)
+from timm.data.transforms import RandomResizedCropAndInterpolation
+from transformers.models.wav2vec2.modeling_wav2vec2 import _compute_mask_indices
+
+_pil_interpolation_to_str = {
+    Image.NEAREST: 'PIL.Image.NEAREST',
+    Image.BILINEAR: 'PIL.Image.BILINEAR',
+    Image.BICUBIC: 'PIL.Image.BICUBIC',
+    Image.LANCZOS: 'PIL.Image.LANCZOS',
+    Image.HAMMING: 'PIL.Image.HAMMING',
+    Image.BOX: 'PIL.Image.BOX',
+}
+
+
+def _pil_interp(method):
+    if method == 'bicubic':
+        return Image.BICUBIC
+    elif method == 'lanczos':
+        return Image.LANCZOS
+    elif method == 'hamming':
+        return Image.HAMMING
+    else:
+        # default bilinear, do we want to allow nearest?
+        return Image.BILINEAR
+
+
+_RANDOM_INTERPOLATION = (Image.BILINEAR, Image.BICUBIC)
+
+
+class MIMTransform(object):
+    """
+    Masked Image Modeling transforms based on BEiT. copied from https://github.com/microsoft/unilm/tree/master/beit
+    """
+
+    def __init__(self, patch_size=16, num_patches=14, num_mask_patches=120, input_size=224, min_num_patches=16,
+                 max_num_patches=196,
+                 interpolation="bicubic", imagenet_mean_and_std=False):
+        imagenet_default_mean_and_std = imagenet_mean_and_std  # cfg.imagenet_default_mean_and_std
+        mean = IMAGENET_INCEPTION_MEAN if not imagenet_default_mean_and_std else IMAGENET_DEFAULT_MEAN
+        std = IMAGENET_INCEPTION_STD if not imagenet_default_mean_and_std else IMAGENET_DEFAULT_STD
+        self.patch_size = patch_size
+        self.num_patches = num_patches
+        self.num_mask_patches = num_mask_patches
+        self.input_size = input_size
+        assert self.patch_size * self.num_patches == self.input_size
+        self.min_num_patches = min_num_patches
+        self.max_num_patches = max_num_patches
+        self.interpolation = interpolation
+
+        self.common_transform = transforms.Compose([
+            transforms.ColorJitter(0.4, 0.4, 0.4),
+            transforms.RandomHorizontalFlip(p=0.5),
+            RandomResizedCropAndInterpolation(size=self.input_size, interpolation=self.interpolation),
+            transforms.ToTensor(),
+            transforms.Normalize(mean=torch.tensor(mean), std=torch.tensor(std))
+        ])
+
+        self.masked_position_generator = ImageMaskingGenerator(
+            self.num_patches, num_masking_patches=self.num_mask_patches,
+            max_num_patches=self.max_num_patches,
+            min_num_patches=self.min_num_patches,
+        )
+
+    def __call__(self, image):
+        return self.common_transform(image), self.masked_position_generator()
+
+
+class ImageMaskingGenerator:
+    def __init__(
+            self, input_size, num_masking_patches, min_num_patches=4, max_num_patches=None,
+            min_aspect=0.3, max_aspect=None):
+        if not isinstance(input_size, tuple):
+            input_size = (input_size,) * 2
+        self.height, self.width = input_size
+
+        self.num_masking_patches = num_masking_patches
+
+        self.min_num_patches = min_num_patches
+        self.max_num_patches = num_masking_patches if max_num_patches is None else max_num_patches
+
+        max_aspect = max_aspect or 1 / min_aspect
+        self.log_aspect_ratio = (math.log(min_aspect), math.log(max_aspect))
+
+    def __repr__(self):
+        repr_str = "Generator(%d, %d -> [%d ~ %d], max = %d, %.3f ~ %.3f)" % (
+            self.height, self.width, self.min_num_patches, self.max_num_patches,
+            self.num_masking_patches, self.log_aspect_ratio[0], self.log_aspect_ratio[1])
+        return repr_str
+
+    def get_shape(self):
+        return self.height, self.width
+
+    def _mask(self, mask, max_mask_patches):
+        delta = 0
+        for attempt in range(10):
+            target_area = random.uniform(self.min_num_patches, max_mask_patches)
+            aspect_ratio = math.exp(random.uniform(*self.log_aspect_ratio))
+            h = int(round(math.sqrt(target_area * aspect_ratio)))
+            w = int(round(math.sqrt(target_area / aspect_ratio)))
+            if w < self.width and h < self.height:
+                top = random.randint(0, self.height - h)
+                left = random.randint(0, self.width - w)
+
+                num_masked = mask[top: top + h, left: left + w].sum()
+                # Overlap
+                if 0 < h * w - num_masked <= max_mask_patches:
+                    for i in range(top, top + h):
+                        for j in range(left, left + w):
+                            if mask[i, j] == 0:
+                                mask[i, j] = 1
+                                delta += 1
+
+                if delta > 0:
+                    break
+        return delta
+
+    def __call__(self):
+        mask = torch.zeros(self.get_shape(), dtype=torch.int)
+        mask_count = 0
+        while mask_count < self.num_masking_patches:
+            max_mask_patches = self.num_masking_patches - mask_count
+            max_mask_patches = min(max_mask_patches, self.max_num_patches)
+
+            delta = self._mask(mask, max_mask_patches)
+            if delta == 0:
+                break
+            else:
+                mask_count += delta
+
+        return mask
+
+
+class AudioMaskingGenerator:
+    def __init__(self,
+                 mask_prob: float,
+                 mask_length: int,
+                 attention_mask=None,
+                 min_masks: int = 0):
+        self.mask_prob = mask_prob
+        self.mask_length = mask_length
+        self.attention_mask = attention_mask
+        self.min_masks = min_masks
+
+    def __call__(self, shape):
+        batch_size, audio_size = shape
+        mask = _compute_mask_indices((batch_size, audio_size),
+                                     self.mask_prob,
+                                     self.mask_length,
+                                     self.attention_mask,
+                                     self.min_masks)
+        mask = torch.from_numpy(mask)
+        return mask
+
+
+def generate_masked_tensor(input_tensor, mask, fill=0):
+    masked_tensor = torch.zeros(input_tensor.size(), device=input_tensor.device) + fill
+    masked_tensor[mask] = input_tensor[mask]
+    return masked_tensor
+
+
+if __name__ == "__main__":
+    transform = MIMTransform()
+
+    num_patches = 14
+    num_mask_patches = 120
+    max_num_patches = 196
+    min_num_patches = 16
+
+    video_mask_generator = ImageMaskingGenerator(input_size=num_patches,
+                                                 num_masking_patches=num_mask_patches,
+                                                 max_num_patches=max_num_patches,
+                                                 min_num_patches=min_num_patches)
+    audio_mask_generator = AudioMaskingGenerator(mask_prob=0.065,
+                                                 mask_length=10,
+                                                 attention_mask=None,
+                                                 min_masks=1)
+    print("Done!")
---
+++ data2vec/utils/trainer.py
@@ -0,0 +1,181 @@
+import math
+import torch
+from torch import nn, optim
+from typing import Callable, Tuple
+from torch.utils.data import DataLoader
+from data2vec.utils.misc import log, save_model
+import os
+import time
+from tqdm import tqdm
+
+
+def compute_var(y):
+    y = y.view(-1, y.size(-1))
+    return torch.sqrt(y.var(dim=0) + 1e-6).mean()
+
+
+def train_single_batch(net: nn.Module, data: torch.Tensor, mask: torch.Tensor, optimizer: optim.Optimizer, criterion: Callable, device: torch.device) -> Tuple[float, int]:
+    """Performs a single training step.
+
+    Args:
+        net (nn.Module): Model instance.
+        data (torch.Tensor): Data tensor, of shape (batch_size, dim_1, ... , dim_N).
+        targets (torch.Tensor): Target tensor, of shape (batch_size).
+        optimizer (optim.Optimizer): Optimizer instance.
+        criterion (Callable): Loss function.
+        device (torch.device): Device.
+
+    Returns:
+        float: Loss scalar.
+        int: Number of correct preds.
+    """
+
+    data = data.to(device)
+
+    optimizer.zero_grad()
+    predictions, targets = net(data, data, mask)
+    scale = math.sqrt(predictions.size(dim=-1))
+    loss = criterion(predictions.float(), targets.float()).sum(dim=-1).sum().div(scale)
+    loss.backward()
+    optimizer.step()
+    with torch.no_grad():
+        target_var = compute_var(targets.float())
+        prediction_var = compute_var(predictions.float())
+    return loss.item(), target_var.item(), prediction_var.item()
+
+
+@torch.no_grad()
+def evaluate(net: nn.Module, mask_generator, criterion: Callable, dataloader: DataLoader, device: torch.device) -> Tuple[float, float]:
+    """Performs inference.
+
+    Args:
+        net (nn.Module): Model instance.
+        criterion (Callable): Loss function.
+        dataloader (DataLoader): Test or validation loader.
+        device (torch.device): Device.
+
+    Returns:
+        accuracy (float): Accuracy.
+        float: Loss scalar.
+    """
+
+    net.eval()
+    correct = 0
+    running_loss = 0.0
+    running_target_var = 0.0
+    running_prediction_var = 0.0
+
+    for data in tqdm(dataloader):
+        data = data.to(device)
+        batch_size = data.size(dim=0)
+        audio_length = data.size(dim=-1)
+        mask = mask_generator(shape=(batch_size, audio_length)).to("cuda")
+        mask = torch.cat([torch.zeros(batch_size, 1, device=mask.device), mask], dim=1).bool()
+
+        
+        predictions, targets = net(data, data, mask)
+        scale = math.sqrt(predictions.size(dim=-1))
+        loss = criterion(predictions.float(), targets.float()).sum(dim=-1).sum().div(scale)
+        target_var = compute_var(targets.float())
+        prediction_var = compute_var(predictions.float())
+        running_loss += loss.item()
+        running_target_var += target_var.item()
+        running_prediction_var += prediction_var.item()
+        
+    net.train()
+    avg_loss = running_loss / len(dataloader.dataset)
+    avg_target_var = running_target_var / len(dataloader)
+    avg_prediction_var = running_prediction_var / len(dataloader)
+    return avg_loss, avg_target_var, avg_prediction_var
+
+
+def train(net: nn.Module, mask_generator, optimizer: optim.Optimizer, criterion: Callable, trainloader: DataLoader, valloader: DataLoader, schedulers: dict, config: dict) -> None:
+    """Trains model.
+
+    Args:
+        net (nn.Module): Model instance.
+        optimizer (optim.Optimizer): Optimizer instance.
+        criterion (Callable): Loss function.
+        trainloader (DataLoader): Training data loader.
+        valloader (DataLoader): Validation data loader.
+        schedulers (dict): Dict containing schedulers.
+        config (dict): Config dict.
+    """
+    
+    step = 0
+    best_avg_loss = 0.0
+    n_batches = len(trainloader)
+    device = config["hparams"]["device"]
+    log_file = os.path.join(config["exp"]["save_dir"], "training_log.txt")
+    
+    ############################
+    # start training
+    ############################
+    net.train()
+    
+    for epoch in range(config["hparams"]["n_epochs"]):
+        t0 = time.time()
+        running_loss = 0.0
+        running_target_var = 0.
+        running_prediction_var = 0.
+        for batch_index, data, in enumerate(trainloader):
+            batch_size = data.size(dim=0)
+            audio_length = data.size(dim=-1)
+            if schedulers["warmup"] is not None and epoch < config["hparams"]["scheduler"]["n_warmup"]:
+                schedulers["warmup"].step()
+
+            elif schedulers["scheduler"] is not None:
+                schedulers["scheduler"].step()
+
+            ####################
+            # optimization step
+            ####################
+            mask = mask_generator(shape=(batch_size, audio_length)).to("cuda")
+            mask = torch.cat([torch.zeros(batch_size, 1, device=mask.device), mask], dim=1).bool()
+
+            loss, target_var, prediction_var = train_single_batch(net, data, mask, optimizer, criterion, device)
+            net.ema_step()
+            running_loss += loss
+            running_target_var += target_var
+            running_prediction_var += prediction_var
+
+            if not step % config["exp"]["log_freq"]:       
+                log_dict = {"epoch": epoch, "loss": loss, "lr": optimizer.param_groups[0]["lr"],
+                            "target_var": target_var, "prediction_var": prediction_var}
+                log(log_dict, step, config)
+
+            step += 1
+            
+        #######################
+        # epoch complete
+        #######################
+
+        log_dict = {"epoch": epoch, "time_per_epoch": time.time() - t0, "train_target_var": running_target_var/n_batches, 
+                    "train_prediction_var": running_prediction_var/n_batches, "avg_loss_per_ep": running_loss/len(trainloader.dataset)}
+        log(log_dict, step, config)
+
+        if not epoch % config["exp"]["val_freq"]:
+            avg_val_loss, avg_val_target_var, avg_val_prediction_var = evaluate(net, mask_generator, criterion, valloader, device)
+            log_dict = {"epoch": epoch, "val_loss": avg_val_loss,
+                        "avg_val_target_var": avg_val_target_var, "avg_val_prediction_var": avg_val_prediction_var}
+            log(log_dict, step, config)
+
+            # save best val ckpt
+            if avg_val_loss < best_avg_loss:
+                best_avg_loss = avg_val_loss
+                save_path = os.path.join(config["exp"]["save_dir"], "best.pth")
+                save_model(epoch, avg_val_loss, save_path, net, optimizer, log_file) 
+
+    ###########################
+    # training complete
+    ###########################
+
+    avg_val_loss, avg_val_target_var, avg_val_prediction_var = evaluate(net, mask_generator, criterion, valloader, device)
+    log_dict = {"epoch": epoch, "val_loss": avg_val_loss,
+                "avg_val_target_var": avg_val_target_var, "avg_val_prediction_var": avg_val_prediction_var}
+           
+    log(log_dict, step, config)
+
+    # save final ckpt
+    save_path = os.path.join(config["exp"]["save_dir"], "last.pth")
+    save_model(epoch, avg_val_loss, save_path, net, optimizer, log_file)
